16c2575c9e01e043f3a79cc34d4b93aab3f9fd51
sand <shmsand@gmail.com>
2019-07-21 15:10:59 +0800

    update web page

--- visualization/assets/js/visualization06.js
+++ visualization/assets/js/visualization06.js
@@ -1,69 +1,47 @@
-function getXYFromTranslate(element) {
-  var split = element.getAttribute("transform").split(" ");
-  var x = parseFloat(split[4]);
-  var y = parseFloat(split[5].split(")")[0]);
-  return [x, y];
-}
 
 document.addEventListener("DOMContentLoaded", function () {
-  var counter = 0;
-
-  var children = document.querySelector("g#circle").children;
-  var text_positions = []
-  document.querySelectorAll("g#text>text").forEach(text => {
-    text_positions.push(getXYFromTranslate(text));
-  })
-
-
-  set1 = new Set([]);
-  set2 = new Set([]);
-  for (var i = 0; i < document.querySelectorAll("g#text>text").length; i++) {
-    set2.add(i);
-  }
-  var texts = []
-  document.querySelectorAll("g#text>text").forEach((text, index) => {
-    text.setAttribute("id", index);
-    texts.push(text.textContent);
-  });
-  Array.from(document.querySelector("g#circle").children).forEach(circle => {
-    var cx = parseFloat(circle.getAttribute("cx"));
-    var cy = parseFloat(circle.getAttribute("cy"));
-    var distance = Number.MAX_VALUE;
-    var index = 0;
-    var i = 0;
-    text_positions.forEach(position => {
-      var x = position[0];
-      var y = position[1];
-      var currentDis = Math.sqrt(Math.pow(cx - x, 2) + Math.pow(cy - y, 2))
-      // console.log(currentDis,distance)
-      if (distance > currentDis) {
-        index = i;
-        distance = currentDis;
-      }
-      i++;
+  var div = d3
+    .select('#unicorn')
+    .append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
+  d3.selectAll("g#circle circle").nodes().forEach(node => {
+    d3.select(node).on("mouseover", function (d) {
+      this.setAttribute('style', 'fill-opacity:.25;');
+      div
+        .transition()
+        .duration(200)
+        .style('opacity', 0.9);
+      var name = this.getAttribute('data');
+      // var nDomin = data[0];
+      // var nDate = data[1];
+      // var nodeData = domainData[nDomin];
+      // var country = nodeData['country'];
+      // var type = nodeData['type'];
+      // var ranking = nodeData[nDate];
+      // var text =
+      //   nDomin +
+      //   '<br>' +
+      //   country +
+      //   '<br>' +
+      //   type +
+      //   '<br>' +
+      //   nDate +
+      //   ' ranking: ' +
+      //   ranking;
+      div
+        .html(name)
+        .style('left', d3.event.pageX + 5 + 'px')
+        .style('top', d3.event.pageY - 20 + 'px');
     })
-
-
-    circle.setAttribute("data", index);
-    circle.addEventListener("mouseover", (event) => {
-      // console.log("hover")
-      console.log(event)
-      var id = parseInt(circle.getAttribute("data"));
-      text = document.querySelectorAll("g#text>text")[id];
-      console.log(text)
-      text.setAttribute("style", "")
-    })
-    set1.add(index);
-    circle.setAttribute("text", texts[index]);
-
-
+      .on('mouseout', function (d) {
+        this.setAttribute('style', 'fill-opacity:1;');
+        div
+          .transition()
+          .duration(500)
+          .style('opacity', 0);
+      });
   })
-  var difference = new Set([...set2].filter(x => !set1.has(x)));
-  for (let i of difference) {
-    console.log(i, texts[i])
-
-  }
-
 });
 
 // document.querySelectorAll("svg > g").forEach(e => { id = e.getAttribute("id"); if (id.includes("age") || id.includes("edu" )) { e.setAttribute("style", "opacity:0;") } })
--- visualization/assets/js/visualization07.js
+++ visualization/assets/js/visualization07.js
@@ -1,7 +1,47 @@
 
 document.addEventListener("DOMContentLoaded", function () {
- 
-
+  var div = d3
+    .select('#unicorn02')
+    .append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
+  d3.selectAll("g#circle circle").nodes().forEach(node => {
+    d3.select(node).on("mouseover", function (d) {
+      this.setAttribute('style', 'fill-opacity:.25;');
+      div
+        .transition()
+        .duration(200)
+        .style('opacity', 0.9);
+      var name = this.getAttribute('data');
+      // var nDomin = data[0];
+      // var nDate = data[1];
+      // var nodeData = domainData[nDomin];
+      // var country = nodeData['country'];
+      // var type = nodeData['type'];
+      // var ranking = nodeData[nDate];
+      // var text =
+      //   nDomin +
+      //   '<br>' +
+      //   country +
+      //   '<br>' +
+      //   type +
+      //   '<br>' +
+      //   nDate +
+      //   ' ranking: ' +
+      //   ranking;
+      div
+        .html(name)
+        .style('left', d3.event.pageX + 5 + 'px')
+        .style('top', d3.event.pageY - 20 + 'px');
+    })
+      .on('mouseout', function (d) {
+        this.setAttribute('style', 'fill-opacity:1;');
+        div
+          .transition()
+          .duration(500)
+          .style('opacity', 0);
+      });
+  })
 });
 
 // document.querySelectorAll("svg > g").forEach(e => { id = e.getAttribute("id"); if (id.includes("age") || id.includes("edu" )) { e.setAttribute("style", "opacity:0;") } })
